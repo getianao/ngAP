9ee98f4fad5a6d907373fe108c7da2916a26dc5e
getianao <getianao@gmail.com>
2024-12-08 20:25:04 +0800

    Update script

---
+++ scripts/run-throughput-extend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+time ${NGAP_ROOT}/scripts/run_throughput_gpu_nap_defalut_e1.sh
+time ${NGAP_ROOT}/scripts/run_throughput_gpu_nap_defalut_e2.sh
+time ${NGAP_ROOT}/scripts/run_throughput_gpu_nap_defalut_e2p.sh
+
+time ${NGAP_ROOT}/scripts/run_throughput_gpu_nap_defalut.sh
